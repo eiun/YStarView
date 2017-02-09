dd6c8f98061014fda0d0a71b6c46b45dbab96eab
eiun <302899658@qq.com>
2017-02-09 11:37:23 +0800

    YStarView
    基础版本

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # YStarView
+
+
+使用方法
+xml的：
+    <com.eiun.hesy.mystar.YStarView
+        android:id="@+id/star"
+        android:layout_width="60dp"
+        android:layout_height="180dp" />
+只要宽高，自己调就好
+
+Activity里面找到控件后：
+        star.setStarCount(5);//星星总数
+        star.setRating(3);//设置星星亮的颗数
+        star.setChange(true);//设置星星是否可以点击和滑动改变
+        star.setStar(R.drawable.ic_ystar_t,R.drawable.ic_ystar_d);// 设置星星的样式
+
+        star.getRating()//获取亮星星的颗数
+不设置就会使用默认的
+不能再简单了·····
---
+++ star/app/src/main/java/com/eiun/hesy/mystar/MainActivity.java
@@ -0,0 +1,49 @@
+package com.eiun.hesy.mystar;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * 类名称：MainActivity<br>
+ * 内容摘要： //主页。<br>
+ * 属性描述：<br>
+ * 方法描述：<br>
+ * 修改备注：   <br>
+ * 创建时间： 2017/2/8 10:10<br>
+ *
+ * @author eiun<br>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private YStarView star;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+        star.setStarCount(5);
+        star.setRating(3);
+        star.setChange(true);
+        star.setStar(R.drawable.ic_ystar_t,R.drawable.ic_ystar_d);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(textView, star.getRating()+"颗", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initView() {
+        star = (YStarView) findViewById(R.id.star);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+}
